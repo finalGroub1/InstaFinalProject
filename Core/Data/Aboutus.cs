102a5d5a3373722f0fce00dbca92e2e7aac8ac86
HayaMasabha <hayamasabha@gmail.com>
2022-08-16 01:23:56 +0300

    Aboutus table added

---
+++ Core/Data/Aboutus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Data
+{
+    class Aboutus
+    {
+        public int id { get; set; }
+        public string description1 { get; set; }
+        public string description2 { get; set; }
+        public string imge1 { get; set; }
+        public string imge2 { get; set; }
+
+    }
+}
