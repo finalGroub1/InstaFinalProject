85473e85c4f0112973356bfa7c1ae8c8e756f824
HayaMasabha <hayamasabha@gmail.com>
2022-08-16 01:20:46 +0300

    contact us table added

---
+++ Core/Data/Contactus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Data
+{
+    public class Contactus
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string massege { get; set; }
+        public string phonenumber { get; set; }
+        public string email { get; set; }
+    }
+}
