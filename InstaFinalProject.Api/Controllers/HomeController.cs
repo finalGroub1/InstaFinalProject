d7693c9bc924d1d93ab798fb5492fd42dd281b65
HayaMasabha <hayamasabha@gmail.com>
2022-08-21 01:06:44 +0300

    HomeController

--- InstaFinalProject.Api/Controllers/HomeController.cs
+++ InstaFinalProject.Api/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -50,5 +51,63 @@ namespace InstaFinalProject.Api.Controllers
         {
             return homeService.updateHome(home);
         }
+
+
+        #region UploadImage1
+
+        [Route("uploadImage1")]
+        [HttpPost]
+        public Home UploadImage()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                var fullPath = Path.Combine("Src", fileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                Home item = new Home();
+                item.imge1 = fileName;
+                return item;
+
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+
+        }
+
+        #endregion
+
+        #region UploadImage2
+        [Route("uploadImage2")]
+        [HttpPost]
+        public Home UploadImage2()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                var fullPath = Path.Combine("Src", fileName);
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                Home item = new Home();
+                item.imge2 = fileName;
+                return item;
+
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+        #endregion
     }
 }
