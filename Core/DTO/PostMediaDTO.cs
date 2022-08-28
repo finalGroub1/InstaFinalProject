92fc65aefa51bcd854d168aa8a5dbaeb48bac154
HayaMasabha <hayamasabha@gmail.com>
2022-08-29 00:57:21 +0300

    .

---
+++ Core/DTO/PostMediaDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DTO
+{
+   public class PostMediaDTO
+    {
+        public int id { get; set; }
+        public DateTime createdate { get; set; }
+        public int state { get; set; }
+        public string description { get; set; }
+        public int position { get; set; }
+        public int user_id { get; set; }
+        public string name { get; set; }
+        public string mediapath { get; set; }
+    }
+}
